5d3f69ea2c79e74d8a6b48097e63213eb2714d8a
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-07-18 00:20:28 +0530

    Add module check

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const express = require('express')
-const app = express()
-const PORT = 6060
+const express = require("express");
+const app = express();
+const PORT = 6060;
 
-app.get('/', (req, res) => {
-  res.send(`<div>It's a Sample Node App</div>`)
-})
+app.get("/", (req, res) => {
+  res.send(`<div>It's a Sample Node App</div>`);
+});
 
-app.get('/app', (req, res) => {
-    res.send(`GET Request`)
-})
+app.get("/app", (req, res) => {
+  res.send(`GET Request`);
+});
 
-app.post('/app', (req, res) => {
-    res.send(`POST Request`)
-})
+app.post("/app", (req, res) => {
+  res.send(`POST Request`);
+});
 
-app.patch('/app', (req, res) => {
-    res.send(`PATCH Request`)
-})
+app.patch("/app", (req, res) => {
+  res.send(`PATCH Request`);
+});
 
-app.put('/app', (req, res) => {
-    res.send(`PUT Request`)
-})
+app.put("/app", (req, res) => {
+  res.send(`PUT Request`);
+});
 
-app.delete('/app', (req, res) => {
-    res.send(`DELETE Request`)
-})
+app.delete("/app", (req, res) => {
+  res.send(`DELETE Request`);
+});
 
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`Node running at ${PORT} | Test at http://localhost:${PORT}/`) 
-})
+// Check if the module is being run directly
+if (require.main === module) {
+  app.listen(process.env.PORT || PORT, () => {
+    console.log(`Node running at ${PORT} | Test at http://localhost:${PORT}/`);
+  });
+} else {
+  // Export the app instance for importing
+  module.exports = app;
+}
